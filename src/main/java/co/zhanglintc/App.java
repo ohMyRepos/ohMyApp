11c29f3e39116f359e2e4aa7a7cff5bd47f2862e
zhanglintc <zhanglintc623@gmail.com>
2024-01-16 15:34:52 +0800

    getClassLoader

--- src/main/java/co/zhanglintc/App.java
+++ src/main/java/co/zhanglintc/App.java
@@ -1,13 +1,16 @@
 package co.zhanglintc;
 
+import java.io.File;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        System.out.println(App.class.getClassLoader().getResource(""));
+        System.out.println(App.class.getResource(""));
         System.out.println( "Hello World!" );
     }
 }
