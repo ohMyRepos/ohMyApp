ee271bb5911c8747a10ecbcf7d001d34822cc70b
zhanglintc <zhanglintc623@gmail.com>
2024-08-14 00:04:52 +0800

    log

--- src/main/java/co/zhanglintc/App.java
+++ src/main/java/co/zhanglintc/App.java
@@ -4,14 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * Hello world!
- *
  */
 @Slf4j
 public class App {
-    public static void main( String[] args ) {
-        log.info("Hello World!");
+    public static void main(String[] args) {
+        log.info("start: Hello World!");
         System.out.println(App.class.getClassLoader().getResource(""));
         System.out.println(App.class.getResource(""));
-        System.out.println( "Hello World!" );
+        log.info("end: Hello World!");
     }
 }
