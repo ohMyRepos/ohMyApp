110b34d4410a47589cbef1dd71f6a67db53bb62d
zhanglintc <zhanglintc623@gmail.com>
2024-08-13 23:59:08 +0800

    log4j2

--- src/main/java/co/zhanglintc/App.java
+++ src/main/java/co/zhanglintc/App.java
@@ -1,12 +1,15 @@
 package co.zhanglintc;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+@Slf4j
+public class App {
     public static void main( String[] args ) {
+        log.info("Hello World!");
         System.out.println(App.class.getClassLoader().getResource(""));
         System.out.println(App.class.getResource(""));
         System.out.println( "Hello World!" );
